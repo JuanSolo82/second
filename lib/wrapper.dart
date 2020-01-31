8586caa11c737dbe2bc3afab6606d1e47857c25e
jmarinan <jmarinan@tabsa.cl>
2020-01-31 16:42:47 -0300

    flutter firebase

---
+++ lib/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:second/screens/authenticate/authenticate.dart';
+//import 'package:second/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Authenticate();
+  }
+}
